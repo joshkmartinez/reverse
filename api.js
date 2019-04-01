cca118b31ad6d1e0d4eff6d3699bff32b517c280
Joshua Martinez <joshkmartinez@gmail.com>
2019-04-01 08:58:11 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,22 +1,10 @@
 var express = require('express')
 var app = express()
-const { parse } = require('url')
 const api = express.Router()
 function reverse(str) {
-  // Step 1. Use the split() method to return a new array
-  var splitString = str.split('') // var splitString = "hello".split("");
-  // ["h", "e", "l", "l", "o"]
-
-  // Step 2. Use the reverse() method to reverse the new created array
-  var reverseArray = splitString.reverse() // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-  // ["o", "l", "l", "e", "h"]
-
-  // Step 3. Use the join() method to join all elements of the array into a string
-  var joinArray = reverseArray.join('') // var joinArray = ["o", "l", "l", "e", "h"].join("");
-  // "olleh"
-
-  //Step 4. Return the reversed string
-  return joinArray // "olleh"
+  var splitString = str.split('')
+  var reverseArray = splitString.reverse()
+  return reverseArray.join('')
 }
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
